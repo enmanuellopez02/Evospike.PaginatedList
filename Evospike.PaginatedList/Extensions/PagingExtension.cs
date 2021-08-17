d33a56b792308d8e706de8dd65bfbf0b4345a256
Enmanuel López <enmanuellopez02@hotmail.com>
2021-08-17 19:51:19 -0400

    Update PagingExtension.cs

--- Evospike.PaginatedList/Extensions/PagingExtension.cs
+++ Evospike.PaginatedList/Extensions/PagingExtension.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Evospike.PaginatedList.Models;
@@ -20,6 +21,29 @@ namespace Evospike.PaginatedList.Extensions
             {
                 Items = await query.Skip(page).Take(take).ToListAsync(),
                 TotalItems = await query.CountAsync(),
+                Page = originalPages
+            };
+
+            if (result.TotalItems > 0)
+            {
+                result.TotalPages = Convert.ToInt32(Math.Ceiling(Convert.ToDecimal(result.TotalItems) / take));
+            }
+
+            return result;
+        }
+
+        public static DataCollection<T> GetPaged<T>(this List<T> source, int page, int take)
+        {
+            var originalPages = page;
+            page--;
+
+            if (page > 0)
+                page *= take;
+
+            var result = new DataCollection<T>
+            {
+                Items = source.Skip(page).Take(take).ToList(),
+                TotalItems = source.Count,
                 Page = originalPages
             };
 
